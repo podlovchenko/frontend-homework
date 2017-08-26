e96b8f47c6efd42f908ea2fae4a2a07944a37d44
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-26 12:15:19 +0300

    Update euclid.js

--- source/euclid.js
+++ source/euclid.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const euclid = function (...args) {
-	const min = Math.min.apply(Math, args);
+	args = args.map(item => Math.abs(item));
+	args.sort((a,b) => a - b);
+
+	let min = 0;
+
+	for (let i = 0; i < args.length; i++) {
+		if(args[i] !== 0) {
+		min = args[i];
+		break;
+		}
+	}
+
+	if (min === 0) {
+		return;
+	}
 
 	for (let i = min; i > 1; i--) {
 		let state = true;
